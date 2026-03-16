f317a2ab49e0b8328307ce36a6802e10e6d59d45
PeanSass <seanspacer123@gmail.com>
2026-03-16 16:31:14 -0500

    Finished dictionary.

--- my-guitar-app/src/util/berkleeDictionary.js
+++ my-guitar-app/src/util/berkleeDictionary.js
@@ -49,8 +49,152 @@ export const berkleeDictionary = {
         ]
       }
     ],
-    // type2: [],
-    // type3: [],
-    // type4: []
+    type2: [ // No Stretches
+      {
+        string: 0, // Low E (6th string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 2, offset: 1 }, // Root note is here
+          { finger: 4, offset: 3 }
+        ]
+      },
+      {
+        string: 1, // A (5th string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 2, offset: 1 },
+          { finger: 4, offset: 3 } 
+        ]
+      },
+      {
+        string: 2, // D (4th string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 3, offset: 2 },
+          { finger: 4, offset: 3 }
+        ]
+      },
+      {
+        string: 3, // G (3rd string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 3, offset: 2 },
+          { finger: 4, offset: 3 }
+        ]
+      },
+      {
+        string: 4, // B (2nd string)
+        notes: [
+          { finger: 2, offset: 1 },
+          { finger: 4, offset: 3 }
+        ]
+      },
+      {
+        string: 5, // High E (1st string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 2, offset: 1 },
+          { finger: 4, offset: 3 }
+        ]
+      }
+    ],
+    type3: [ // No Stretches
+      {
+        string: 0, // Low E (6th string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 3, offset: 2 },
+          { finger: 4, offset: 3 } 
+        ]
+      },
+      {
+        string: 1, // A (5th string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 3, offset: 2 },
+          { finger: 4, offset: 3 } // Root note is here
+        ]
+      },
+      {
+        string: 2, // D (4th string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 2, offset: 2 },
+          { finger: 4, offset: 3 }
+        ]
+      },
+      {
+        string: 3, // G (3rd string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 3, offset: 2 },
+        ]
+      },
+      {
+        string: 4, // B (2nd string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 2, offset: 1 },
+          { finger: 4, offset: 3 }
+        ]
+      },
+      {
+        string: 5, // High E (1st string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 2, offset: 1 },
+          { finger: 4, offset: 3 }
+        ]
+      }
+    ],
+    type4: [ // All out-of-position scale tones played with pink stretches
+      {
+        string: 0, // Low E (6th string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 3, offset: 2 },
+          { finger: 4, offset: 3 } // Root note is here
+        ]
+      },
+      {
+        string: 1, // A (5th string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 3, offset: 2 },
+          { finger: 4, offset: 3 }
+        ]
+      },
+      {
+        string: 2, // D (4th string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 2, offset: 2 },
+          { finger: 4, offset: 4 } // Pinky Stretch
+        ]
+      },
+      {
+        string: 3, // G (3rd string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 3, offset: 2 }
+        ]
+      },
+      {
+        string: 4, // B (2nd string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 2, offset: 1 },
+          { finger: 4, offset: 3 }
+        ]
+      },
+      {
+        string: 5, // High E (1st string)
+        notes: [
+          { finger: 1, offset: 0 },
+          { finger: 3, offset: 2 },
+          { finger: 4, offset: 3 }
+        ]
+      }
+    ]
   }
 };
