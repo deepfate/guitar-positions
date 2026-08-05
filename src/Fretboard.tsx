5520fef2ca04911c214b0f2757a61244b30bd1de
deepfate <seanspacer123@gmail.com>
2026-08-05 20:13:51 +0000

    Started debugging Fretboard.tsx

--- src/Fretboard.tsx
+++ src/Fretboard.tsx
@@ -69,7 +69,7 @@
 
 import React, { useMemo, useState, useRef } from 'react';
 import { generateFretboard, FretNode } from './util/Fretboard'; // Adjust path as needed
-import { berkleeDictionary, rootDefinitions, FingeringType } from './util/berkleeDictionary';
+//import { berkleeDictionary, rootDefinitions, FingeringType } from './util/berkleeDictionary';
 import { Scale, Note } from '@tonaljs/tonal';
 import './Fretboard.css';
 import ControlPanel from './ControlPanel';
